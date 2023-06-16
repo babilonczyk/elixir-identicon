784d7257aae3c76d0ed822591298faec300f01f9
Jan Piotrzkowski <piotrzkowski.jan@gmail.com>
2023-06-16 14:14:07 +0200

    Add descriptions

--- lib/elixir_identicon.ex
+++ lib/elixir_identicon.ex
@@ -3,6 +3,10 @@ defmodule ElixirIdenticon do
   Documentation for `ElixirIdenticon`.
   """
 
+  @doc """
+  Generates an identicon from the given input (string).
+  Identicon is saved in images directory under the passed input.
+  """
   def generate(input) do
     input
     |> hash_input
@@ -14,6 +18,9 @@ defmodule ElixirIdenticon do
     |> save(input)
   end
 
+  @doc """
+  Hashes the input and returns an image struct with a seed.
+  """
   defp hash_input(input) do
     seed =
       :crypto.hash(:sha256, input)
@@ -22,11 +29,17 @@ defmodule ElixirIdenticon do
     %ElixirIdenticon.Image{seed: seed}
   end
 
+  @doc """
+  Adds a color to the image struct, based on the first 3 values of the seed.
+  """
   def set_color(image) do
     [r, g, b | _tail] = image.seed # %ElixirIdenticon.Image{seed: [r, g, b | _]) = image
     %ElixirIdenticon.Image{image | color: %{red: r, green: g, blue: b}}
   end
 
+  @doc """
+  Generates a grid of 7x4, based on the seed. Adds the index to each value. Returns an image struct with the new grid.
+  """
   def generate_grid(image) do
     grid =
       image.seed
@@ -39,11 +52,17 @@ defmodule ElixirIdenticon do
     %ElixirIdenticon.Image{image | grid: grid}
   end
 
+  @doc """
+  Generates a grid of 7x7, miroring horizonatly the first 3 values in each row.
+  """
   def mirror_row(row) do
     [num1, num2, num3 | _tail] = row
     row ++ [num3, num2, num1]
   end
 
+  @doc """
+  Normalzies the grid by removing all enums with odd values.
+  """
   def normalize_grid(image) do
     grid =
       image.grid
@@ -52,6 +71,9 @@ defmodule ElixirIdenticon do
     %ElixirIdenticon.Image{image | grid: grid}
   end
 
+  @doc """
+  Creates a pixel map based on the grid. Returns an image struct with the new pixel map.
+  """
   def generate_pixel_map(image) do
     pixel_map = Enum.map image.grid, fn({_val, index}) ->
         x = rem(index,7) * 100
@@ -65,6 +87,9 @@ defmodule ElixirIdenticon do
     %ElixirIdenticon.Image{image | pixel_map: pixel_map}
   end
 
+  @doc """
+  Draws an image based on the pixel map. Returns a binary.
+  """
   def draw_image(%ElixirIdenticon.Image{color: color, pixel_map: pixel_map }) do
     canvas = :egd.create(700, 700)
     color = :egd.color({color.red, color.green, color.blue})
@@ -76,6 +101,9 @@ defmodule ElixirIdenticon do
     :egd.render(canvas)
   end
 
+  @doc """
+  Saves image from memory to the file.
+  """
   def save(image, name) do
     File.write!("images/#{name}.png", image)
   end
--- lib/image.ex
+++ lib/image.ex
@@ -1,3 +1,10 @@
 defmodule ElixirIdenticon.Image do
+  @moduledoc """
+  Documentation for `ElixirIdenticon.Image`.
+  """
+
+  @doc """
+  Struct of an image.
+  """
   defstruct seed: nil, color: nil, grid: nil, pixel_map: nil
 end
